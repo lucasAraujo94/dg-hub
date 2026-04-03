964376b92908b7d7a5a612c027b294d6136c4df0
lucasAraujo94 <98675611+lucasAraujo94@users.noreply.github.com>
2026-04-03 18:44:46 -0300

    Add files via upload

--- client/src/const.ts
+++ client/src/const.ts
@@ -1,4 +1,4 @@
-export { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
+export { COOKIE_NAME, ONE_YEAR_MS } from "../../shared/const";
 
 const normalizePath = (value: string) => {
   const trimmed = value.trim();
