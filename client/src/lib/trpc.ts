fe7e321d726e8d488f856ae964302a38ccbcf847
Lucas Araujo da Silva <luaraujo@sefaz.ba.gov.br>
2026-04-07 10:57:59 -0300

    Ensure single tRPC client instance

--- client/src/lib/trpc.ts
+++ client/src/lib/trpc.ts
@@ -1,6 +1,10 @@
 import { createTRPCReact } from "@trpc/react-query";
 import type { AppRouter } from "../../../server/routers";
 
-export const trpc = createTRPCReact<AppRouter>();
+// Ensure a single TRPC React client instance even if the module is bundled twice.
+const createTRPC = () => createTRPCReact<AppRouter>();
+const TRPC_GLOBAL_KEY = "__DG_TRPC_SINGLETON__";
+const globalAny = globalThis as unknown as Record<string, ReturnType<typeof createTRPC> | undefined>;
 
-
+export const trpc = globalAny[TRPC_GLOBAL_KEY] ?? createTRPC();
+globalAny[TRPC_GLOBAL_KEY] = trpc;
